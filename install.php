238123fc65dec73095f518a825af74e632fc3428
Stefan Nyman <stefan@jumpstarter.io>
2015-08-21 17:26:32 +0200

    update to 1.8.1

--- install.php
+++ install.php
@@ -64,10 +64,6 @@ function wp_install($blog_title, $user_name, $user_email, $public, $deprecated =
 
 	wp_cache_flush();
 
-	if (isset($_SERVER['SERVER_SOFTWARE']) && stripos($_SERVER['SERVER_SOFTWARE'], 'apache') !== false || isset($_SERVER['SERVER_SIGNATURE']) && stripos($_SERVER['SERVER_SIGNATURE'], 'apache') !== false) {
-		;// Your server is Apache. Nothing to do more.
-	}
-
 	return array('url' => $guessurl, 'user_id' => $user_id, 'password' => $user_password, 'password_message' => $message);
 }
 ?>
--- pdodb.class.php
+++ pdodb.class.php
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
 	die();
 }
 require_once PDODIR . 'pdoengine.class.php';
-require_once PDODIR . 'install.php';
+//require_once PDODIR . 'install.php';
 
 if (!defined('SAVEQUERIES')){
 	define ('SAVEQUERIES', false);
@@ -211,8 +211,6 @@ class PDODB extends wpdb {
 			$this->bail(sprintf(__("<h1>Error establlishing a database connection</h1><p>We have been unable to connect to the specified database. <br />The error message received was %s"), $this->dbh->errorInfo()));
 			return;
 		}
-		$is_enabled_foreign_keys = @$this->get_var('PRAGMA foreign_keys');
-		if ($is_enabled_foreign_keys == '0') @$this->query('PRAGMA foreign_keys = ON');
 		$this->has_connected = true;
 		$this->ready = true;
 	}
